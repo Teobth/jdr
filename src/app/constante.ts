29309441b322fdc29a1db80a934e13275a0e5cd7
Teo <teobth@gmail.com>
2026-06-25 15:51:42 +0200

    Push avant Sécurisation

--- src/app/constante.ts
+++ src/app/constante.ts
@@ -16,4 +16,8 @@ export const WS_BASE_URL: string = 'ws://192.168.1.49:3000';
 //Partage de co
 // export const WS_BASE_URL: string = 'ws://10.90.238.23:3000';
 
+//Suisse
+//export const WS_BASE_URL : string = 'ws://192.168.1.55:3000';
+
+
 export const IP_ADDRESS = WS_BASE_URL.slice(5,17)
